ea7f0a6b6e9fa4bef955c404f3009e4f0b5ce2b8
hmcdcarlos1408 <117197233+hmcdcarlos1408@users.noreply.github.com>
2022-12-07 01:38:37 -0600

    master

--- components/Profile.js
+++ components/Profile.js
@@ -20,31 +20,50 @@ const LoginForm = ({navigation}) => {
        <NativeBaseProvider>
     <Center>
     <StatusBar bg="#3700B3" barStyle="light-content"/> 
-    <Box safeAreaTop bg="blue.400" /> 
-    <HStack bg="blue.400" px="1" py="3" justifyContent="space-between" alignItems="center" w="100%" maxW="350">
-      <HStack alignItems="center" justifyContent="space-between">
-          <Button bgColor="blue.400" >
-          <AntDesign name="bars" style={{ color: 'white', fontSize: 35 }}/>
-          </Button>
-          
-        <Text color="white" fontSize="20" fontWeight="bold">
-          Pagina principal 
-        </Text>
-      </HStack>
-      <HStack>
-          <Button bgColor="blue.400" >
-              <MaterialCommunityIcons name="account" style={{ color: 'white', fontSize: 35 }}/>
-          </Button>
-      </HStack>
-      
-    </HStack>
-      <br/>
+
     <HStack>
+    <Box w="90%" alignItems="center">
+      <Menu w="190" trigger={triggerProps => {
+       <Pressable accessibilityLabel="More options menu" {...triggerProps}>
+              <HamburgerIcon />
+            </Pressable>;
+    }}>
+        <Menu.Item
+  onPress= {()=>{
+    navigation.navigate('Profile'
+       
+    )}}
+  >Home</Menu.Item>
+        <Menu.Item onPress= {()=>{
+    navigation.navigate('informacion'
+       
+    )}}>Informacion</Menu.Item>
+
+
+        <Menu.Item onPress= {()=>{
+    navigation.navigate('informacion'
+       
+    )}}>Inf. Fiscal</Menu.Item>
+
+
+
+        <Menu.Item onPress= {()=>{
+    navigation.navigate('I_IEPS'
+       
+    )}} > Inf.IEPS</Menu.Item>
+        <Menu.Item>SF Pro</Menu.Item>
+        <Menu.Item>Helvetica</Menu.Item>
+        <Menu.Item isDisabled>Sofia</Menu.Item>
+        <Menu.Item>Cookie</Menu.Item>
+      </Menu>
+    </Box>
+  
+  
       <Input w="100%" flex={1} size="xl" variant="underlined" placeholder="¿Que deseas buscar?"> 
       </Input>
       <Button variant="unstyled" width="20" height="55"> <AntDesign name="search1" style={{ color: 'black', fontSize: 35 }}/> </Button>  
     </HStack>
-    <br/>
+   
     <Box p="2" width="350" height="500" bgColor="lightBlue.100" rounded="xl" shadow={2}>
         <Text size="2xl"> ¡Bienvenido a MySATapp! </Text>
         <Text size="lg"> Info de la aplicacion </Text>
